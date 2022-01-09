215c2233e678f43beb6e35b49a5a90346d8bc8bc
yaraureluis <yaraureluis@gmail.com>
2022-01-09 18:46:00 -0300

    se agregó Nav basico

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Tu Saldo VA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Preguntas Frecuentes</Nav.Link>
+              <NavDropdown title="Planes" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Telefonia</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Televisión</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#link">Contacto</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+export default NavBar;
