7a8dfb9aa6ecc826955d38d07951ac26bd084a5d
yaraureluis <yaraureluis@gmail.com>
2022-03-03 18:16:27 -0300

    agregada function toggle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,76 +7,81 @@ import { cartContext } from "./CartContext";
 function NavBar() {
   const { cart } = useContext(cartContext);
 
+  function toggle() {
+    let toggle = document.getElementById("toggle");
+    toggle.click();
+  }
+
   return (
     <>
-      <Navbar style={{ backgroundColor: "#16254f", borderBottom: "3px solid orange" }} expand="lg" variant={"dark"}>
+      <Navbar collapseOnSelect style={{ backgroundColor: "#16254f", borderBottom: "3px solid orange" }} expand="lg" variant={"dark"}>
         <Container>
           <Navbar.Brand>
             <NavLink activeClassName="linkActivo" className="linkPropio" to="/">
               <img src="/imagenes/logo_blanco.svg" alt="logo_marca" height={"40px"} />
             </NavLink>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" id="toggle" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <NavLink activeClassName="linkActivo" className="linkPropio paddingNav" to="/preguntas">
+              <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio paddingNav" to="/preguntas">
                 Preguntas Frecuentes
               </NavLink>
               <NavDropdown title="Categorías" id="basic-nav-dropdown">
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" to="/categoria/telefonia">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" to="/categoria/telefonia">
                     Telefonia
                   </NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" to="/categoria/television">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" to="/categoria/television">
                     Televisión
                   </NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" exact to="/categoria/todas">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" exact to="/categoria/todas">
                     Ver todas
                   </NavLink>
                 </NavDropdown.Item>
               </NavDropdown>
               <NavDropdown title="Operadoras" id="basic-nav-dropdown">
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" to="/operadora/digitel">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" to="/operadora/digitel">
                     Digitel
                   </NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" to="/operadora/movistar">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" to="/operadora/movistar">
                     Movistar
                   </NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" exact to="/operadora/movilnet">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" exact to="/operadora/movilnet">
                     Movilnet
                   </NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" to="/operadora/inter">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" to="/operadora/inter">
                     Inter
                   </NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" to="/operadora/simple_tv">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" to="/operadora/simple_tv">
                     Simple TV
                   </NavLink>
                 </NavDropdown.Item>
                 <NavDropdown.Item>
-                  <NavLink activeClassName="linkActivo" className="linkPropio" exact to="/operadora/movistar_tv">
+                  <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio" exact to="/operadora/movistar_tv">
                     Movistar TV
                   </NavLink>
                 </NavDropdown.Item>
               </NavDropdown>
-              <NavLink activeClassName="linkActivo" className="linkPropio paddingNav" to="/contacto">
+              <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio paddingNav" to="/contacto">
                 Contacto
               </NavLink>
             </Nav>
             <Nav>
-              <NavLink activeClassName="linkActivo" className="linkPropio paddingNav" to="/carrito">
+              <NavLink onClick={() => toggle()} activeClassName="linkActivo" className="linkPropio paddingNav" to="/carrito">
                 {cart.length > 0 && <CartWidget />}
               </NavLink>
             </Nav>
